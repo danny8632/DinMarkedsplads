ee55cef68c99ce03e92f54549eef62e14c706d7f
Danny Haslund <dannyhasl1@gmail.com>
2020-10-26 11:53:08 +0100

    added varddump

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ $redirect = explode('?',$_SERVER['REQUEST_URI']); // You can also use $_SERVER['
 
 $redirect_exploded = explode("/", $redirect[0]);
 
+var_dump($redirect_exploded);
+
 if($redirect_exploded[1] != "" && $redirect_exploded[1] == "api_v1")
 {
     switch ($redirect_exploded[2]) {
