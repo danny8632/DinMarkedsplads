b861d27d839490e9d7ce7c34a04c6cbdf9237bcb
Danny Haslund <dannyhasl1@gmail.com>
2020-11-02 10:12:21 +0100

    hot fix

--- api/products/create.php
+++ api/products/create.php
@@ -39,9 +39,9 @@ class Create extends Api {
 
         for ($i=0; $i < count($files['name']); $i++) {
 
-            $path = "assets/fileupload/" . date("Y-m-d_H:i:s") . "_" . basename($files["name"][$i]);
+            $path = "assets".DIRECTORY_SEPARATOR."fileupload". DIRECTORY_SEPARATOR . date("Y-m-d_H:i:s") . "_" . basename($files["name"][$i]);
 
-            if(move_uploaded_file($files["tmp_name"][$i], __DIR__."/../../".$path)) array_push($files_path, $path);   
+            if(move_uploaded_file($files["tmp_name"][$i], $path)) array_push($files_path, $path);   
         }
 
         $this->conn = $this->getDbConn();
